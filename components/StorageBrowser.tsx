c95252e9ef2247e19c28ab2503f3bed98e75b8da
maxim.tyurin <maxim.tyurin@1win.pro>
2025-02-03 18:12:21 +0500

    f

--- components/StorageBrowser.tsx
+++ components/StorageBrowser.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createAmplifyAuthAdapter, createStorageBrowser } from '@aws-amplify/ui-react-storage/browser';
-import '@aws-amplify/ui-react-storage/styles.css';
+import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
 import config from '../amplify_outputs.json';
 
